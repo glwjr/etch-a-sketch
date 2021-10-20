a45e93bf02c2209bdcdaefdb3c1bf6da90e41a4a
Gary White <glw494@gmail.com>
2021-10-20 18:31:58 -0400

    Added clear and resize functionality

--- app.js
+++ app.js
@@ -8,11 +8,33 @@ function createGrid(numCells) {
         container.appendChild(cell);
 
         cell.addEventListener("mouseover", () => handleHover(cell));
+
+        const clearButton = document.getElementById("clear-button");
+        clearButton.addEventListener("click", () => clearGrid(cell));
     }
 }
 
 function handleHover(cell) {
     cell.style.backgroundColor = "black";
+}
+
+function clearGrid(cell) {
+    cell.style.backgroundColor = "white";
+}
+
+const resizeButton = document.getElementById("resize-button");
+resizeButton.addEventListener("click", () => resize());
+
+function resize() {
+    let size = prompt("Please enter a number between 1-64.")
+
+    if (parseInt(size) > 64) {
+        alert("Please enter a number less than 65.")
+        return
+    } else {
+        container.innerHTML = "";
+        createGrid(size);
+    }
 }
 
 createGrid(16);
